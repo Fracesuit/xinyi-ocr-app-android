3d334f996dd185f459d7e0de91954243c2a37c21
zhiren.zhang <915924056@qq.com>
2018-10-31 17:50:27 +0800

    自定义相机识别身份证成功

--- app/src/main/java/cn/net/xinyi/xmjt/MainActivity.java
+++ app/src/main/java/cn/net/xinyi/xmjt/MainActivity.java
@@ -1,5 +1,6 @@
 package cn.net.xinyi.xmjt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,7 +8,9 @@ import android.view.View;
 
 import com.blankj.utilcode.util.ActivityUtils;
 import com.camera.picturelib.CamersActivity;
-import com.kernal.passportreader.sdk.IdcardOrcActivity;
+import com.kernal.passport.sdk.utils.Devcode;
+import com.kernal.passport.sdk.utils.SharedPreferencesHelper;
+import com.kernal.passportreader.sdk.CameraActivity;
 import com.kernal.plateid.PlateOcrActivity;
 
 import butterknife.ButterKnife;
@@ -27,7 +30,26 @@ public class MainActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tv_zjsb:
-                ActivityUtils.startActivity(this, IdcardOrcActivity.class);
+                /**
+                 * intent = new Intent(IdCardMainActivity.this,
+                 CameraActivity.class);
+                 intent.putExtra("nMainId", SharedPreferencesHelper.getInt(
+                 getApplicationContext(), "nMainId", 2));
+                 intent.putExtra("devcode", devcode);
+                 intent.putExtra("flag", 0);
+                 intent.putExtra("nCropType", 0);
+                 intent.putExtra("VehicleLicenseflag", 2);
+                 */
+                Intent intent = new Intent(this,
+                        CameraActivity.class);
+                intent.putExtra("nMainId", SharedPreferencesHelper.getInt(
+                        getApplicationContext(), "nMainId", 2));//
+                intent.putExtra("devcode", Devcode.devcode);
+                intent.putExtra("flag", 0);
+                intent.putExtra("nCropType", 0);
+                intent.putExtra("VehicleLicenseflag", 2);
+                ActivityUtils.startActivity(intent);
+                //ActivityUtils.startActivity(this, IdcardOrcActivity.class);
                 break;
             case R.id.tv_cpsb:
                 ActivityUtils.startActivity(this, PlateOcrActivity.class);
--- picturelib/src/main/java/com/camera/picturelib/CamersActivity.java
+++ picturelib/src/main/java/com/camera/picturelib/CamersActivity.java
@@ -1,10 +1,15 @@
 package com.camera.picturelib;
 
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Bitmap;
 import android.graphics.RectF;
 import android.hardware.Camera;
 import android.media.ExifInterface;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +24,9 @@ import com.blankj.utilcode.util.ToastUtils;
 import com.camera.configuration.Configuration;
 import com.camera.internal.manager.impl.Camera1Manager;
 import com.camera.widgets.cover.SuperCoverView;
+import com.kernal.passport.sdk.utils.Devcode;
+import com.kernal.passportreader.sdk.CameraActivity;
+import com.xinyi_tech.comm.log.XinYiLog;
 import com.xinyi_tech.comm.permission.DefaultRequestPermissionsListener;
 import com.xinyi_tech.comm.permission.PermissionsHelp;
 import com.xinyi_tech.comm.util.FileUtils2;
@@ -29,6 +37,9 @@ import java.io.File;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import kernal.idcard.android.RecogParameterMessage;
+import kernal.idcard.android.RecogService;
+import kernal.idcard.android.ResultMessage;
 
 public class CamersActivity extends AppCompatActivity {
 
@@ -36,7 +47,7 @@ public class CamersActivity extends AppCompatActivity {
     LiveCameraView liveCameraView;
     View sbtn_takephoto;
     private File outputPath;
-
+    RecogService.recogBinder recogBinder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);//  hiding titles
@@ -55,6 +66,8 @@ public class CamersActivity extends AppCompatActivity {
                 });
             }
         });
+        Intent recogIntent = new Intent(CamersActivity.this, RecogService.class);
+        bindService(recogIntent, recogConn, Service.BIND_AUTO_CREATE);
         //权限
         PermissionsHelp.with(this)
                 .requestPermissions(new DefaultRequestPermissionsListener() {
@@ -94,5 +107,57 @@ public class CamersActivity extends AppCompatActivity {
         } catch (Throwable error) {
             Log.e("CamersActivity", "Can't save exif info: " + error.getMessage());
         }*/
+
+          RecogParameterMessage rpm = new RecogParameterMessage();
+         rpm.nTypeLoadImageToMemory = 0;
+         rpm.nMainID = 2;
+         rpm.nSubID = null;
+         rpm.GetSubID = true;
+         rpm.GetVersionInfo = true;
+         rpm.logo = "";
+         rpm.userdata = "";
+         rpm.sn = "";
+         rpm.authfile = "";
+         rpm.isSaveCut = true;
+         rpm.triggertype = 0;
+         rpm.devcode = Devcode.devcode;
+         rpm.isOnlyClassIDCard = true;
+         //rpm.isSetIDCardRejectType=false;
+         // rpm.idcardRotateDegree=3;
+         rpm.isAutoClassify = true;
+         rpm.nv21bytes = bytes;
+         rpm.nv21_width = 2560;
+         rpm.nv21_height = 1440;
+         rpm.lpHeadFileName = "";//  save document protrait
+         rpm.lpFileName = outputPath.getPath(); //  If rpm.lpFileName is null, automatic recognition fuction will be executed
+         rpm.cutSavePath = outputPath.getPath();
+        try {
+            ResultMessage resultMessage = recogBinder.getRecogResult(rpm);
+            String[] GetRecogResult = resultMessage.GetRecogResult;
+            XinYiLog.e(GetRecogResult.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public ServiceConnection recogConn = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            recogBinder = null;
+        }
+
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            recogBinder = (RecogService.recogBinder) service;
+
+        }
+
+    };
+
+    @Override
+    protected void onDestroy() {
+        unbindService(recogConn);
+        super.onDestroy();
     }
 }
